16da8df3d731dd945f09f6417bc025f376ed3f1b
AlexMarrer <alex.uscata80@gmail.com>
2026-04-30 13:03:35 +0200

    feat: add basic setup with controllers

---
+++ src/main/java/uscata/alex/recipevault/category/CategoryController.java
@@ -0,0 +1,9 @@
+package uscata.alex.recipevault.category;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+}
---
+++ src/main/java/uscata/alex/recipevault/ingredient/IngredientController.java
@@ -0,0 +1,9 @@
+package uscata.alex.recipevault.ingredient;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ingredient")
+public class IngredientController {
+}
