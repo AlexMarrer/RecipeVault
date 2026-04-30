95b080e8fd61f3e7350c83277f27f27aa9783a54
AlexMarrer <alex.uscata80@gmail.com>
2026-04-30 11:10:40 +0200

    init

---
+++ src/test/java/uscata/alex/recipevault/RecipevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package uscata.alex.recipevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
