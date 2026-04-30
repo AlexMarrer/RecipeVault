dd3c7d2ef0defb685590824be3756fe1cf6d1acc
AlexMarrer <alex.uscata80@gmail.com>
2026-04-30 14:46:01 +0200

    feat: add secutity config with swagger support and role mapping

---
+++ src/main/java/uscata/alex/recipevault/security/Roles.java
@@ -0,0 +1,7 @@
+package uscata.alex.recipevault.security;
+
+public class Roles {
+    public static final String Admin = "admin";
+    public static final String Chef = "chef";
+    public static final String User = "user";
+}
